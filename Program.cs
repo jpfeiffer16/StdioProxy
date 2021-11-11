ef6456223b68bc877ab4844308faefb44ff3d3f8
jpfeiffer16 <joepfeiffer16@gmail.com>
2021-11-11 14:31:28 -0500

    Split out logfiles

--- Program.cs
+++ Program.cs
@@ -14,15 +14,26 @@ namespace stdioproxy
         private static string HomeFolder = Environment.GetEnvironmentVariable(IsWindows ? "USERPROFILE" : "HOME");
 
         private const int MAX_BUFFER_SIZE = 2048;
-        private static string LOG_FILE = $"{HomeFolder}/stdioproxy.log";
+        private static string OUT_LOG_FILE = $"{HomeFolder}/stdioproxy.stdout.log";
+        private static string IN_LOG_FILE = $"{HomeFolder}/stdioproxy.stdin.log";
+        private static string ERR_LOG_FILE = $"{HomeFolder}/stdioproxy.stderr.log";
         private static string OMNISHARP_EXECUTABLE = IsWindows
             ? $"{HomeFolder}/.omnisharp/omnisharp-roslyn/omnisharp/OmniSharp.exe"
             : $"{HomeFolder}/.cache/omnisharp-vim/omnisharp-roslyn/omnisharp/OmniSharp.exe";
 
         static void Main(string[] args)
         {
-            File.Exists(LOG_FILE) && File.Delete(LOG_FILE);
-            using (var logStream = new StreamWriter(File.Create(LOG_FILE)))
+            if (File.Exists(OUT_LOG_FILE))
+            {
+                File.Delete(OUT_LOG_FILE);
+            }
+            if (File.Exists(IN_LOG_FILE))
+            {
+                File.Delete(IN_LOG_FILE);
+            }
+            using (var stdoutLogStream = new StreamWriter(File.Create(OUT_LOG_FILE)))
+            using (var stdinLogStream = new StreamWriter(File.Create(IN_LOG_FILE)))
+            using (var stderrLogStream = new StreamWriter(File.Create(ERR_LOG_FILE)))
             using (var proc = Process.Start(new ProcessStartInfo
             {
                 FileName = IsWindows ? OMNISHARP_EXECUTABLE : "/usr/bin/mono",
@@ -41,7 +52,21 @@ namespace stdioproxy
                         {
                             var output = new String(buffer.Take(bytesRead).ToArray());
                             Console.Write(output);
-                            logStream.Write(output);
+                            stdoutLogStream.Write(output);
+                        }
+                    }
+                });
+
+                var stdErrTask = Task.Run(() =>
+                {
+                    using (var stderr = proc.StandardError)
+                    {
+                        var buffer = new char[MAX_BUFFER_SIZE];
+                        int bytesRead;
+                        while((bytesRead = stderr.Read(buffer, 0, 1)) > 0)
+                        {
+                            var output = new String(buffer.Take(bytesRead).ToArray());
+                            stderrLogStream.Write(output);
                         }
                     }
                 });
@@ -55,6 +80,7 @@ namespace stdioproxy
                         while ((byteRead = thisStdin.ReadByte()) > 0)
                         {
                             proc.StandardInput.Write((char)byteRead);
+                            stderrLogStream.Write(((char)byteRead).ToString());
                         }
                     }
                 });
